9505f23cb4ea33784092df7ae086f9072ac8c5f0
Spdy999 <1a1a1a.262626.0010@gmail.com>
2021-07-23 17:42:03 +0700

    initial commit

---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
