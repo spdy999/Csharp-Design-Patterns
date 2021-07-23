a30ad06397c6a0046204e58b8672bd3072155e31
Spdy999 <1a1a1a.262626.0010@gmail.com>
2021-07-23 22:31:28 +0700

    feat(op-clo): Open-Closed filter combination

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -6,18 +6,23 @@ namespace DesignPatterns
 {
     public enum Color
     {
-        Red, Green, Blue
+        Red,
+        Green,
+        Blue
     }
 
     public enum Size
     {
-        Small, Medium, Large, Huge
+        Small,
+        Medium,
+        Large,
+        Huge
     }
 
     public class Product
     {
-        public string Name;
         public Color Color;
+        public string Name;
         public Size Size;
 
         public Product(string name, Color color, Size size)
@@ -35,12 +40,13 @@ namespace DesignPatterns
         {
             return products.Where(product => product.Size == size);
         }
-        
+
         public IEnumerable<Product> FilterByColor(IEnumerable<Product> products,
             Color color)
         {
             return products.Where(product => product.Color == color);
         }
+
         public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products,
             Size size, Color color)
         {
@@ -60,14 +66,48 @@ namespace DesignPatterns
 
     public class ColorSpecification : ISpecification<Product>
     {
-        private Color color;
+        private readonly Color color;
+
         public ColorSpecification(Color color)
         {
             this.color = color;
         }
+
         public bool IsSatisfied(Product t)
         {
             return t.Color == color;
+        }
+    }
+
+    public class SizeSpecification : ISpecification<Product>
+    {
+        private readonly Size size;
+
+        public SizeSpecification(Size size)
+        {
+            this.size = size;
+        }
+
+        public bool IsSatisfied(Product t)
+        {
+            return t.Size == size;
+        }
+    }
+
+    public class AndSpecification<T> : ISpecification<T>
+    {
+        private readonly ISpecification<T> first;
+        private readonly ISpecification<T> second;
+
+        public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+
+        public bool IsSatisfied(T t)
+        {
+            return first.IsSatisfied(t) && second.IsSatisfied(t);
         }
     }
 
@@ -78,7 +118,7 @@ namespace DesignPatterns
             return items.Where(spec.IsSatisfied);
         }
     }
-    
+
     internal class Program
     {
         public static void Main(string[] args)
@@ -98,6 +138,16 @@ namespace DesignPatterns
             Console.WriteLine("Green products (new):");
             foreach (var product in bf.Filter(products, new ColorSpecification(Color.Green)))
                 Console.WriteLine($" - {product.Name} is green");
+
+            Console.WriteLine("Large blue items");
+            foreach (var product in bf.Filter(products,
+                new AndSpecification<Product>(
+                    new ColorSpecification(Color.Blue),
+                    new SizeSpecification(Size.Large)
+                )))
+            {
+                Console.WriteLine($" - {product.Name} is big and blue");
+            }
         }
     }
 }
