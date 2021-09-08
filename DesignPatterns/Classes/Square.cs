05e6fd87ebcee16ae791648fd458b646895362de
Spdy999 <1a1a1a.262626.0010@gmail.com>
2021-09-09 05:04:03 +0700

    feat(lis-sub): create Square class which inherit from Rectangle class

---
+++ DesignPatterns/Classes/Square.cs
@@ -0,0 +1,15 @@
+namespace DesignPatterns.Classes
+{
+    public class Square : Rectangle
+    {
+        public new int Width
+        {
+            set => base.Width = base.Height = value;
+        }
+
+        public new int Height
+        {
+            set => base.Width = base.Height = value;
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -5,12 +5,19 @@ namespace DesignPatterns
 {
     internal static class Program
     {
-        public static int Area(Rectangle rc) => rc.Width * rc.Height;
+        public static int Area(Rectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+
         public static void Main(string[] args)
         {
-            Rectangle rc = new Rectangle(2, 3);
+            var rc = new Rectangle(2, 3);
             Console.WriteLine($"{rc} has area {Area(rc)}"); // Width: 2, Height: 3 has area 6
 
+            var sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}"); // Width: 4, Height: 4 has area 16
         }
     }
 }
