56fa89583b5fc25ed8bea60ea67ae71d1c44184d
Spdy999 <1a1a1a.262626.0010@gmail.com>
2021-09-09 04:42:55 +0700

    feat(lis-sub): create Rectangle class

---
+++ DesignPatterns/Classes/Rectangle.cs
@@ -0,0 +1,23 @@
+namespace DesignPatterns.Classes
+{
+    public class Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle()
+        {
+            
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+}
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace DesignPatterns
+﻿using DesignPatterns.Classes;
+
+namespace DesignPatterns
 {
     internal static class Program
     {
+        public static int Area(Rectangle rc) => rc.Width * rc.Height;
         public static void Main(string[] args)
         {
+            Rectangle rc = new Rectangle();
         }
     }
 }
