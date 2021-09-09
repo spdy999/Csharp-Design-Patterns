9a496f1eb6dd0568ada6e74ba008eb208314271a
Spdy999 <1a1a1a.262626.0010@gmail.com>
2021-09-10 04:06:04 +0700

    feat(sin-res): create Journal class

---
+++ DesignPatterns/Classes/Journal.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Classes
+{
+    public class Journal
+    {
+        private static int _count;
+
+        // Properties
+        private readonly List<string> _entries = new List<string>();
+
+        // Methods
+        public int AddEntry(string text)
+        {
+            _entries.Add($"{++_count}");
+            return _count; // memento 
+        }
+
+        public void RemoveEntry(int index)
+        {
+            _entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, _entries);
+        }
+    }
+}
