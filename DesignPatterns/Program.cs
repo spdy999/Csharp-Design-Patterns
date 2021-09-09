dbbecb3433ca69175bf45bb8c01e2d73ea5e6c5c
Spdy999 <1a1a1a.262626.0010@gmail.com>
2021-09-09 12:13:35 +0700

    feat(lis-sub): try Rectangle superclass ref type with Square subclass

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -5,7 +5,7 @@ namespace DesignPatterns
 {
     internal static class Program
     {
-        public static int Area(Rectangle rc)
+        private static int Area(Rectangle rc)
         {
             return rc.Width * rc.Height;
         }
@@ -15,9 +15,9 @@ namespace DesignPatterns
             var rc = new Rectangle(2, 3);
             Console.WriteLine($"{rc} has area {Area(rc)}"); // Width: 2, Height: 3 has area 6
 
-            var sq = new Square();
+            Rectangle sq = new Square();
             sq.Width = 4;
-            Console.WriteLine($"{sq} has area {Area(sq)}"); // Width: 4, Height: 4 has area 16
+            Console.WriteLine($"{sq} has area {Area(sq)}"); // Width: 4, Height: 0 has area 0
         }
     }
 }
