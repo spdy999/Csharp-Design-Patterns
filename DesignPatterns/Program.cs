759ae3f6c653416688692eb6c9d7d41c9270aacb
Spdy999 <1a1a1a.262626.0010@gmail.com>
2021-09-09 17:31:06 +0700

    feat(dep-inv): use Relationships class with Person class

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace DesignPatterns
+﻿using DesignPatterns.Classes;
+
+namespace DesignPatterns
 {
     internal static class Program
     {
         public static void Main(string[] args)
         {
+            var parent = new Person() { Name = "John"};
+            var child1 = new Person() { Name = "Chris"};
+            var child2 = new Person() { Name = "Mary"};
+
+            var relationships = new Relationships();
+            relationships.AddParentAndChild(parent, child1);
+            relationships.AddParentAndChild(parent, child2);
         }
     }
 }
